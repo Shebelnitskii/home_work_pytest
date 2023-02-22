b8b9b1e39740aa5a043f3a8164b92514dda2cce6
Shebelnitskiy <Shebelnitskiywork@gmail.com>
2023-02-22 16:30:10 +0400

    add tests

--- test/test_arrs.py
+++ test/test_arrs.py
@@ -9,9 +9,15 @@ def test_get_2():
     with pytest.raises(IndexError):
         assert arrs.get([], 0, "test") == "test"
 
+def test_get_3():
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+
 
 def test_slice_1():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
 
 def test_slice_2():
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+def test_slice_3():
+    assert arrs.my_slice([], 1) == []
