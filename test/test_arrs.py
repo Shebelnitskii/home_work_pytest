d986c4d1e83fe0b5f1e918e5748d5c18b2e5dc21
Shebelnitskiy <Shebelnitskiywork@gmail.com>
2023-02-22 15:43:21 +0400

    Initial commit

---
+++ test/test_arrs.py
@@ -0,0 +1,17 @@
+import pytest
+from utils import arrs
+
+
+def test_get_1():
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+
+def test_get_2():
+    with pytest.raises(IndexError):
+        assert arrs.get([], 0, "test") == "test"
+
+
+def test_slice_1():
+    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+
+def test_slice_2():
+    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
